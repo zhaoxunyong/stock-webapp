b25c433fea53cc7933dabebf7ec294556e7452ee
dave.zhao <dave.zhao@zerofinance.cn>
2019-08-04 13:25:02 +0800

    Delete unused files

--- backend/mybatisGen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export PATH="/usr/local/bin:/usr/bin:$JAVA_HOME/bin:$MVN_HOME/bin:$PATH"
-
-mvn mybatis-generator:generate
--- backend/toaliyun.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-scp target/stock-webapp-1.0.0-SNAPSHOT.jar root@120.79.159.6:/data/app/
